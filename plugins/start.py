43c28937d9471bd1d620b4ee6dad9b6d1a02dbed
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-03-24 10:56:10 -0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -2,13 +2,11 @@ import os
 import asyncio
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
-
 from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
-from database.database import add_user, del_user, full_userbase, present_user
 from pymongo import MongoClient
 from urllib.parse import quote_plus
 import random
@@ -40,121 +38,96 @@ def save_token_to_db(user_id, token):
     collection.insert_one(token_doc)
 
 # Function to handle /start command
-def start(update, context):
-    user_id = update.message.from_user.id
-    logger.info(f"User {user_id} requested /start")
-
-    # Check if the user already has a token
-    existing_token = collection.find_one({'user_id': user_id})
-
-    if existing_token:
-        token = existing_token['token']
-        logger.info(f"User {user_id} already has token: {token}")
-        update.message.reply_text(f'You already have a token: {token}')
-    else:
-        # Generate token
-        token = generate_token()
-        logger.info(f"Generated new token for user {user_id}: {token}")
-
-        # Save token to database
-        save_token_to_db(user_id, token)
-
-        # Provide token through link
-        bot_username = context.bot.username
-        token_encoded = quote_plus(token)
-        link = f"https://t.me/{bot_username}?start=token_{token_encoded}"
-        update.message.reply_text(f'Use this link to verify: {link}')
-        logger.info(f"Sent verification link to user {user_id}")
+async def provide_verification_link(client: Client, message: Message):
+    user_id = message.from_user.id
+    token = generate_token()
+    save_token_to_db(user_id, token)
+    bot_username = client.get_me().username
+    token_encoded = quote_plus(token)
+    link = f"https://t.me/{bot_username}?start=token_{token_encoded}"
+    await message.reply_text(f'Use this link to verify: {link}')
 
 # Function to verify token
 def verify_token(user_id, token):
     token_doc = collection.find_one({'user_id': user_id, 'token': token})
     return token_doc is not None
 
+# Function to handle verified token
+async def process_verified_token(client: Client, message: Message, token):
+    id = message.from_user.id
+    try:
+        base64_string = token.split(" ", 1)[1]
+    except:
+        return
+    string = await decode(base64_string)
+    argument = string.split("-")
+    if len(argument) == 3:
+        try:
+            start = int(int(argument[1]) / abs(client.db_channel.id))
+            end = int(int(argument[2]) / abs(client.db_channel.id))
+        except:
+            return
+        if start <= end:
+            ids = range(start,end+1)
+        else:
+            ids = []
+            i = start
+            while True:
+                ids.append(i)
+                i -= 1
+                if i < end:
+                    break
+    elif len(argument) == 2:
+        try:
+            ids = [int(int(argument[1]) / abs(client.db_channel.id))]
+        except:
+            return
+    temp_msg = await message.reply("Please wait...")
+    try:
+        messages = await get_messages(client, ids)
+    except:
+        await message.reply_text("Something went wrong..!")
+        return
+    await temp_msg.delete()
+
+    for msg in messages:
+        if bool(CUSTOM_CAPTION) & bool(msg.document):
+            caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
+        else:
+            caption = "" if not msg.caption else msg.caption.html
+
+        if DISABLE_CHANNEL_BUTTON:
+            reply_markup = msg.reply_markup
+        else:
+            reply_markup = None
+
+        try:
+            await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+            await asyncio.sleep(0.5)
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+            await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+        except:
+            pass
+
 # Main code
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
-    token = message.text.split(" ", 1)[1]
-    if verify_token(id, token):
-        # Run your main code here if the token is valid and verified
-        try:
-            base64_string = token.split(" ", 1)[1]
-        except:
-            return
-        string = await decode(base64_string)
-        argument = string.split("-")
-        if len(argument) == 3:
-            try:
-                start = int(int(argument[1]) / abs(client.db_channel.id))
-                end = int(int(argument[2]) / abs(client.db_channel.id))
-            except:
-                return
-            if start <= end:
-                ids = range(start,end+1)
-            else:
-                ids = []
-                i = start
-                while True:
-                    ids.append(i)
-                    i -= 1
-                    if i < end:
-                        break
-        elif len(argument) == 2:
-            try:
-                ids = [int(int(argument[1]) / abs(client.db_channel.id))]
-            except:
-                return
-        temp_msg = await message.reply("Please wait...")
-        try:
-            messages = await get_messages(client, ids)
-        except:
-            await message.reply_text("Something went wrong..!")
-            return
-        await temp_msg.delete()
-
-        for msg in messages:
-
-            if bool(CUSTOM_CAPTION) & bool(msg.document):
-                caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
-            else:
-                caption = "" if not msg.caption else msg.caption.html
-
-            if DISABLE_CHANNEL_BUTTON:
-                reply_markup = msg.reply_markup
-            else:
-                reply_markup = None
-
-            try:
-                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-                await asyncio.sleep(0.5)
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-            except:
-                pass
+    tokens = message.text.split(" ", 1)
+    if len(tokens) > 1:
+        # User provided a token, proceed with verification
+        token = tokens[1]
+        if verify_token(id, token):
+            # Token is valid, proceed with the main code
+            await process_verified_token(client, message, token)
+        else:
+            # Token is not valid or verified, provide instructions
+            await message.reply_text("Invalid token or not verified.")
     else:
-        # Run token system code here if the token is not valid or not verified
-        reply_markup = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 unlock", url="https://shrs.link/FUmxXe")
-                ]
-            ]
-        )
-        await message.reply_text(
-            text = START_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-            reply_markup = reply_markup,
-            disable_web_page_preview = True,
-            quote = True
-        )
+        # User didn't provide a token, provide the verification link
+        await provide_verification_link(client, message)
+
 
 #=====================================================================================##
 

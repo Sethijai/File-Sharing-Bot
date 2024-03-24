8bdf93bde15e654cf40f8b8bb04a46b16e44003b
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-03-24 10:48:25 -0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,8 +9,6 @@ from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
-
-from telegram.ext import Updater, CommandHandler
 from pymongo import MongoClient
 from urllib.parse import quote_plus
 import random
